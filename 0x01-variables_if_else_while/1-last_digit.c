f6daffc67bc1515cc06353ada18caf228f962796
Papaa Jessu <papaajessu@gmail.com>
2023-07-14 17:19:32 +0100

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
 * main - print if the number is postive, zero, or negative
 *
@@ -13,9 +12,11 @@ int main(void)
 {
 int n;
 int l;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 l = n % 10;
+
 if (l > 5)
 {
 	printf("Last digit of %d is %d and is greater than 5\n", n, l);
