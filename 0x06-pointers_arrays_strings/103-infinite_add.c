c56da2e4aac5bc51587589e299e163de37a730fd
Papaa Jessu <papaajessu@gmail.com>
2023-07-29 00:33:17 +0100

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,20 +11,20 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int count, count2;
-	while (n1[count] != '\0')
-		count++;
-	while (n2[count2] != '\0')
-		count2++;
-	*r = *(r + size_r);
-	while (n1[count] > 0 || n1[count2] > 0)
-	{
-		if (n1[count] + n2[count2] > 0)
-			*r = n1[count - 1] + n2[count2 - 1] + 1;
-		else
-			*r = n1[count] + n2[count2];
-		count--;
-		count2--;
-	}
-	return (r);
+int count, count2;
+while (n1[count] != '\0')
+count++;
+while (n2[count2] != '\0')
+count2++;
+*r = *(r + size_r);
+while (n1[count] > 0 || n1[count2] > 0)
+{
+if (n1[count] + n2[count2] > 0)
+*r = n1[count - 1] + n2[count2 - 1] + 1;
+else
+*r = n1[count] + n2[count2];
+count--;
+count2--;
+}
+return (r);
 }
