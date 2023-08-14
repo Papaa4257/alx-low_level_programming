0491cf907f386befce08cb499ff1747f3ad773f8
Papaa Jessu <papaajessu@gmail.com>
2023-08-14 10:25:37 -0700

    Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef head
+#define head
+/**
+ * defining a struct
+ *
+ * name, type = char *
+ * age, type = float
+ * owner, type = char *
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
