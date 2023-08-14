628774aa6654f1eaa99c484db5b8d3ba4c96ae97
Papaa Jessu <papaajessu@gmail.com>
2023-08-14 18:40:44 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,4 +12,10 @@ char *name;
 float age;
 char *owner;
 };
+typedef struct dog dog2;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog2 *new_dog(char *name, float age, char *owner);
+void free_dog(dog2 *d);
 #endif
