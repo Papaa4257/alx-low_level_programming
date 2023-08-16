63cf16688128b198d2d4ab2b7998d4757954be5f
Papaa Jessu <papaajessu@gmail.com>
2023-08-16 12:13:10 +0100

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,8 +4,8 @@
  * print_name - prints a name
  * @name: name to print
  */
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
+if (name != NULL && f != NULL);
 f(name);
 }
