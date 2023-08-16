ff8e05d715644bd5b20a13f8bf24142237daadee
Papaa Jessu <papaajessu@gmail.com>
2023-08-16 11:33:44 +0100

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name to print
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (!name || !f)
+return;
+f(name);
+}
