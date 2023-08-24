b15fc039ce4e9b2926675c5d9a7b70f8d2312795
Papaa Jessu <papaajessu@gmail.com>
2023-08-24 21:38:45 +0100

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,11 +9,11 @@
  */
 size_t list_len(const list_t *h)
 {
-	unsigned int count = 0;
-	while (h != NULL)
-	{
-		h = h->next;
-		count++;
-	}
-	return (count);
+unsigned int count = 0;
+while (h != NULL)
+{
+h = h->next;
+count++;
+}
+return (count);
 }
