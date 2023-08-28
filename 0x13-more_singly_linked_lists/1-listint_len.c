3a90e0d8f60a9cf08edd2a1dfe280e3cc7f74c06
Papaa Jessu <papaajessu@gmail.com>
2023-08-28 20:04:46 +0100

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * @h: linked list of type listint_t to traverse
+ *
+ */
+size_t listint_len(const listint_t *h)
+{
+size_t num = 0;
+while (h)
+{
+num++;
+h = h->next;
+}
+return (num);
+}
