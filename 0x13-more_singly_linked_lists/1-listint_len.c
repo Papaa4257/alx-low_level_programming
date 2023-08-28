2c68cdf0db67cb40aa1489ff7d76c0d6a1e86525
Papaa Jessu <papaajessu@gmail.com>
2023-08-28 20:40:17 +0100

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,6 +2,8 @@
 /**
  * @h: linked list of type listint_t to traverse
  *
+ *Description: returns the number of elements in a linked list
+ * Return: number of elements
  */
 size_t listint_len(const listint_t *h)
 {
